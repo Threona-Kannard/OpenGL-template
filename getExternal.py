1d4f651cd78c71e1a1225a9eb3b5d73c51e379d7
Huỳnh Tấn Phát <66297538+Threona-Kannard@users.noreply.github.com>
2021-12-30 14:11:21 +0700

    Update tool get External and update the source code auto get the shader folder

--- getExternal.py
+++ getExternal.py
@@ -1,30 +1,55 @@
-from git import Repo
-from git import RemoteProgress
-from tqdm import tqdm
+import tool.gitUtils as git
+from colorama import Fore
+import tool.pythonCheck as pc
+import os
+import sys
 import json
+from pathlib import Path
 
-class CloneProgress(RemoteProgress):
-    def __init__(self):
-        super().__init__()
-        self.pbar = tqdm()
+required_module = {
+    'colorama': 'colorama',
+    'gitpython': 'git',
+    'tqdm': 'tqdm'
+}
 
-    def update(self, op_code, cur_count, max_count=None, message=''):
-        self.pbar.total = max_count
-        self.pbar.n = cur_count
-        self.pbar.refresh()
+try:
+    assert(pc.python_version() == 3), "Python version required at least 3.0"
+    print("Python version: ", sys.version)
+except Exception as err:
+    print(err)
+    input("Press the Enter to continue")
+    sys.exit(1)
 
-def main():
-    # Read the file contain external repo
-    file = open("external.json")
-    data = json.load(file)
+print('-----------------------------------------------------')
+for module, module_name in required_module.items():
+    print(f"Checking the module {module}")
+    if pc.is_module_exist(module_name) == False:
+        pc.install_module(module)
+    else:
+        print(
+            f"Module {module} has been installed!!!")
+    print('-----------------------------------------------------')
 
-    for lib in data["external_lib"]:
-        repo_name = lib["repo_name"] 
-        repo_url = lib["repo_url"]
-        clone_location = lib["clone_location"]
-        Repo.clone_from(repo_url,clone_location, progress=CloneProgress())
 
-    file.close()
+# Read the file contain external repo
+file = Path("./external.json")
 
-main()
+try:
+    assert(file.is_file()), Fore.RED+f'The file {file.name} does not exist!'
+except Exception as err:
+    print(err)
+    input('Press Enter to continue')
+    sys.exit(1)
 
+with open(file, 'r') as repo_data:
+    data = json.load(repo_data)
+for lib in data["external_lib"]:
+    repo_name = lib["repo_name"]
+    repo_url = lib["repo_url"]
+    clone_location = lib["clone_location"]
+    if os.path.isdir(clone_location):
+        print(Fore.MAGENTA +
+              f"The folder {repo_name} has been existed" + Fore.RESET)
+    else:
+        git.clone_repo(repo_name, repo_url, clone_location)
+print('-----------------------------------------------------')
--- src/fragment.shader
+++
@@ -1,9 +0,0 @@
-#version 330 core
-out vec4 FragColor;
-
-in vec3 ourColor;
-
-void main()
-{
-    FragColor = vec4(ourColor.x, ourColor.y, ourColor.z, 1.0f);
-}
